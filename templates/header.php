4900f77c7009c56712c8e98e7a6bc0c14f79ded8
SundLyDev <167065053+Sullivankow@users.noreply.github.com>
2024-12-17 22:27:46 +0100

    premier commit/page accueil

--- templates/header.php
+++ templates/header.php
@@ -1,7 +1,3 @@
-<?php
-session_start();
-?>
-
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -9,34 +5,32 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="assets/css/override-bootstrap.css">
+    <link rel="stylesheet" href="assets\css\override-bootstrap.css">
     <title>Okaz</title>
 </head>
+
+
 
 <body>
     <div class="container">
         <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
             <div class="col-md-3 mb-2 mb-md-0">
                 <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
-                    <img width="120" src="assets/images/logo-okaz.png" alt="Logo Okaz">
+                    <img src="assets\images\logo-okaz.png" alt="logo" width="120px">
                 </a>
             </div>
 
             <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-                <li><a href="index.php" class="nav-link px-2 link-secondary">Accueil</a></li>
-                <li><a href="annonces.php" class="nav-link px-2">Annonces</a></li>
+                <li><a href="#" class="nav-link px-2 link-secondary">Home</a></li>
+                <li><a href="#" class="nav-link px-2">Features</a></li>
+                <li><a href="#" class="nav-link px-2">Pricing</a></li>
+                <li><a href="#" class="nav-link px-2">FAQs</a></li>
+                <li><a href="#" class="nav-link px-2">About</a></li>
             </ul>
 
             <div class="col-md-3 text-end">
-                <?php if (isset($_SESSION["user"])): ?>
-                    <span>Bonjour <?= $_SESSION["user"]["username"] ?></span>
-
-                    <a class="btn btn-primary" href="logout.php">Déconnexion</a>
-                <?php else: ?>
-                    <a class="btn btn-outline-primary me-2" href="login.php">Connexion</a>
-                    <a class="btn btn-primary" href="inscription.php">Inscription</a>
-                <?php endif; ?>
+                <button type="button" class="btn btn-outline-primary me-2">Login</button>
+                <button type="button" class="btn btn-primary">Sign-up</button>
             </div>
         </header>
         <main>
--- templates/listing_part.php
+++ templates/listing_part.php
@@ -1,12 +1,10 @@
 <div class="col-md-4 my-2 d-flex">
-    <div class="card w-100">
-        <img src="/uploads/listing/<?= $listing["image"] ?>" class="card-img-top" alt="<?= $listing["title"] ?>">
-        <div class="card-body d-flex flex-column">
+    <div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
             <h5 class="card-title"><?= $listing["title"] ?></h5>
-            <p class="card-text"><?= $listing["price"] ?> €</p>
-            <div class="mt-auto">
-                <a href="annonce.php?id=<?= $listing["id"] ?>" class="btn btn-primary stretched-link w-100">Voir l'annonce</a>
-            </div>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
         </div>
     </div>
 </div>
